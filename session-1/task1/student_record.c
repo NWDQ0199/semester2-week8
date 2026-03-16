7cb4175d3fca5172c5a57b9a792c53d6f3a3e76b
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-03-16 11:10:32 +0000

    sav

--- session-1/task1/student_record.c
+++ session-1/task1/student_record.c
@@ -16,7 +16,8 @@
  * param student The student whose average mark is to be calculated
  * return float The average mark
  */
-float calculate_average(struct student_record student) {
+float calculate_average(struct student_record student)
+{
     // TODO: Implement this function to calculate and return the average
     // of the three module marks
     
@@ -28,7 +29,8 @@ float calculate_average(struct student_record student) {
  * 
  * student The student whose information is to be displayed
  */
-void display_student(struct student_record student) {
+void display_student(struct student_record student)
+{
     // TODO: Implement this function to display student information
     // Format: Name, ID, individual marks, and average mark
 }
@@ -36,11 +38,15 @@ void display_student(struct student_record student) {
 /*
  * main
  */
-int main( void ) {
+int main(void)
+{
     // TODO: Create and initialise a student variable with sample data
     // Example: name = "John Smith", id = 12345, marks = {75.0, 68.5, 81.0}
     
     struct student_record student;
+    student.name="John Smith";
+    student.id=12345;
+    student.marks={75.0,68.5,81.0};
     
     // TODO: display the student data and average mark
     
