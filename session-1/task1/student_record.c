c16af78ee4f396a87889fa64064825616374b724
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-03-16 11:33:24 +0000

    session 1 task1

--- session-1/task1/student_record.c
+++ session-1/task1/student_record.c
@@ -16,7 +16,7 @@
  * param student The student whose average mark is to be calculated
  * return float The average mark
  */
-float calculate_average(struct student_record student)
+float calculate_average(Student student)
 {
 	// TODO: Implement this function to calculate and return the average
 	// of the three module marks
@@ -33,7 +33,7 @@ float calculate_average(struct student_record student)
  * 
  * student The student whose information is to be displayed
  */
-void display_student(struct student_record student)
+void display_student(Student student)
 {
 	// TODO: Implement this function to display student information
 	// Format: Name, ID, individual marks, and average mark
@@ -54,17 +54,22 @@ int main(void)
 	// TODO: Create and initialise a student variable with sample data
 	// Example: name = "John Smith", id = 12345, marks = {75.0, 68.5, 81.0}
 	
-	struct student_record student;
-	strcpy(student.name,"John Smith");
+	Student student=
+	{
+		.name="John Smith",
+		.id=12345,
+		.marks={75.0f,68.5f,81.0f}
+	};
+	/*strcpy(student.name,"John Smith");
 	student.id=12345;
 	float marks[NUM_MODULES]={75.0f,68.5f,81.0f};
 	for(int i=0;i<NUM_MODULES;i++)
 	{
 		//because apparently you can't assign an array in C for some reason
 		student.marks[i]=marks[i];
-	}
+	}*/
 	
 	// TODO: display the student data and average mark
-	
+	display_student(student);
 	return 0;
 }
