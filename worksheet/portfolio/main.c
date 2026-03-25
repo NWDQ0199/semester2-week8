1d4f090e0e274bde65accb9eae0d2d5ff340ba46
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-03-25 17:04:43 +0000

    worksheet 2.8

--- worksheet/portfolio/main.c
+++ worksheet/portfolio/main.c
@@ -1,14 +1,62 @@
-
 #include <stdio.h>
 #include <stdbool.h>
-
 #include "shapes.h"
 
-int main( void ) {
+void testLine(void);
+void testTriangle(void);
+void testLine2(void);
+bool sameFloat(float a, float b);
 
-    Point p1 = makePoint(0.0,0.0);
-    
-    // test your code by calling the functions and printing the output 
-    
-    return 0;
+int main(void)
+{
+	/*
+	a line (1,1),(2,1) will have length 1.0
+	a triangle (0,0),(1,0),(0,1) will have area 0.5
+	a point (1,1) is not part of the previous triangle (false/0)
+	a point (2,2) is part of the line (3,2),(2,2) (true/1)
+	*/
+	// test your code by calling the functions and printing the output 
+	testLine();
+	testTriangle();
+	testLine2();
+
+	return 0;
 }
+
+void testLine(void)
+{
+	//a line (1,1),(2,1) will have length 1.0
+	Point p1=makePoint(1.f,1.f);
+	Point p2=makePoint(2.f,1.f);
+	Line l=makeLine(p1,p2);
+	float length=lineLength(l);
+	printf("|l|=%f",length);
+	printf("testLine: %s",sameFloat(length,1.f)?"pass":"fail");
+}
+
+void testTriangle(void)
+{
+	//a triangle (0,0),(1,0),(0,1) will have area 0.5
+	Point p1=makePoint(0.f,0.f);
+	Point p2=makePoint(1.f,0.f);
+	Point p3=makePoint(0.f,1.f);
+	Triangle t=makeTriangle(p1,p2,p3);
+	float area=triangleArea(t);
+	printf("area(t)=%f",area);
+	printf("testTriangle (a): %s",sameFloat(area,0.5f)?"pass":"fail");
+
+	//a point (1,1) is not part of the previous triangle (false/0)
+	Point p4=makePoint(1.f,1.f);
+	printf("testTriangle (b): %s",pointInTriangle(p4,t)?"fail":"pass");
+}
+
+void testLine2(void)
+{
+	//a point (2,2) is part of the line (3,2),(2,2) (true/1)
+	Point p1=makePoint(3.f,2.f);
+	Point p2=makePoint(2.f,2.f);
+	Line l=makeLine(p1,p2);
+	Point p3=makePoint(2.f,2.f);
+	printf("testLine2: %s",pointInLine(p3,l)?"pass":"fail");
+}
+
--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -4,9 +4,13 @@
 
 #include "shapes.h"
 
-Point makePoint( float x, float y ) {
+bool sameFloat(float a, float b); //would put this in shapes.h but instructed not to modify
+
+Point makePoint(float x, float y)
+{
     Point new;
-    // implementation
+    new.x=x;
+    new.y=y;
     return new;
 }
 
@@ -14,3 +18,57 @@ Point makePoint( float x, float y ) {
 // - start with stubs as above
 // - compile regularly to test syntax
 // - test functions by calling them from main()
+
+Line makeLine(Point p1, Point p2)
+{
+    Line new;
+    new.p[0]=p1;
+    new.p[1]=p2;
+    return new;
+}
+
+Triangle makeTriangle(Point p1, Point p2, Point p3)
+{
+    Triangle new;
+    new.p[0]=p1;
+    new.p[1]=p2;
+    new.p[2]=p3;
+    return new;
+}
+
+float lineLength(Line l)
+{
+    float x1x2=l.p[0].x-l.p[1].x;
+    float y1y2=l.p[0].y-l.p[1].y;
+    return sqrt(x1x2*x1x2+y1y2*y1y2);
+}
+
+float triangleArea(Triangle t)
+{
+    //using Heron's formula
+    float a=lineLength(makeLine(t.p[0],t.p[1]));
+    float b=lineLength(makeLine(t.p[0],t.p[2]));
+    float c=lineLength(makeLine(t.p[1],t.p[2]));
+    float s=(a+b+c)/2.f;
+    return sqrt(s*(s-a)*(s-b)*(s-c));
+}
+
+bool samePoint(Point p1, Point p2)
+{
+    return abs(lineLength(makeLine(p1,p2)))<1.0e-6;
+}
+
+bool pointInLine(Point p, Line l)
+{
+    return samePoint(p,l.p[0])||samePoint(p,l.p[1]);
+}
+
+bool pointInTriangle(Point p, Triangle t)
+{
+    return samePoint(p,t.p[0])||samePoint(p,t.p[1])||samePoint(p,t.p[2]);
+}
+
+bool sameFloat(float a, float b)
+{
+	return abs(a-b)<1.0e-6;
+}
